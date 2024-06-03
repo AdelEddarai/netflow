88ffde3123f28ccc3cfb792dc0ba38ebe5e02a99
unknown <adeleddarai29@gmail.com>
2024-06-03 13:47:30 +0000

    first commit

--- app/[locale]/dashboard/workspace/[workspace_id]/mind-maps/mind-map/[mind_map_id]/page.tsx
+++ app/[locale]/dashboard/workspace/[workspace_id]/mind-maps/mind-map/[mind_map_id]/page.tsx
@@ -10,82 +10,95 @@ import { getAuthSession } from '@/lib/auth';
 import { changeCodeToEmoji } from '@/lib/changeCodeToEmoji';
 import { checkIfUserCompletedOnboarding } from '@/lib/checkIfUserCompletedOnboarding';
 import { Metadata } from 'next';
-import { notFound } from 'next/navigation';
 
 interface Params {
-	params: {
-		workspace_id: string;
-		mind_map_id: string;
-	};
+    params: {
+        workspace_id: string;
+        mind_map_id: string;
+    };
 }
 
 export async function generateMetadata({ params: { mind_map_id } }: Params): Promise<Metadata> {
-	const session = await getAuthSession();
+    const session = await getAuthSession();
 
-	if (!session) notFound();
-	const mindMap = await getMindMap(mind_map_id, session.user.id);
+    if (!session) return { title: 'Error: No session found' };
+    const mindMap = await getMindMap(mind_map_id, session.user.id);
 
-	if (mindMap)
-		return {
-			title: mindMap.title.length > 0 ? mindMap.title : 'Untitled mindMap',
-		};
+    if (mindMap) {
+        return {
+            title: mindMap.title.length > 0 ? mindMap.title : 'Untitled mindMap',
+        };
+    }
 
-	return {};
+    return { title: 'Error: MindMap not found' };
 }
 
 const MindMapPage = async ({ params: { workspace_id, mind_map_id } }: Params) => {
-	const session = await checkIfUserCompletedOnboarding();
+    const session = await checkIfUserCompletedOnboarding();
 
-	const [workspace, userRole, mindMap] = await Promise.all([
-		getWorkspace(workspace_id, session.user.id),
-		getUserWorkspaceRole(workspace_id, session.user.id),
-		getMindMap(mind_map_id, session.user.id),
-	]);
+    if (!session) {
+        return <div>Error: User has not completed onboarding</div>;
+    }
 
-	if (!workspace || !userRole || !mindMap) notFound();
+    const [workspace, userRole, mindMap] = await Promise.all([
+        getWorkspace(workspace_id, session.user.id),
+        getUserWorkspaceRole(workspace_id, session.user.id),
+        getMindMap(mind_map_id, session.user.id),
+    ]);
 
-	const isSavedByUser =
-		mindMap.savedMindMaps?.find((mindMap) => mindMap.userId === session.user.id) !== undefined;
+    if (!workspace) {
+        return <div>Error: Workspace not found</div>;
+    }
+    if (!userRole) {
+        return <div>Error: User role not found</div>;
+    }
+    if (!mindMap) {
+        return <div>Error: MindMap not found</div>;
+    }
 
-	return (
-		<AutosaveIndicatorProvider>
-			<AutoSaveMindMapProvider>
-				<DashboardHeader
-					addManualRoutes={[
-						{
-							name: 'DASHBOARD',
-							href: '/dashboard',
-							useTranslate: true,
-						},
-						{
-							name: workspace.name,
-							href: `/dashboard/workspace/${workspace_id}`,
-						},
-						{
-							emoji: changeCodeToEmoji(mindMap.emoji),
-							name: `${mindMap.title ? mindMap.title : 'UNTITLED'}`,
-							href: '/',
-							useTranslate: mindMap.title ? false : true,
-						},
-					]}>
-					{(userRole === 'ADMIN' || userRole === 'OWNER') && <InviteUsers workspace={workspace} />}
-					<AddTaskShortcut userId={session.user.id} />
-				</DashboardHeader>
-				<main className='flex flex-col gap-2 h-full'>
-					<MindMapPreviewCardWrapper
-						mindMap={mindMap}
-						userRole={userRole}
-						isSavedByUser={isSavedByUser}>
-						<MindMap
-							initialActiveTags={mindMap.tags}
-							initialInfo={mindMap}
-							workspaceId={workspace.id}
-							candEdit={false}
-						/>
-					</MindMapPreviewCardWrapper>
-				</main>
-			</AutoSaveMindMapProvider>
-		</AutosaveIndicatorProvider>
-	);
+    const isSavedByUser = mindMap.savedMindMaps?.find((mindMap) => mindMap.userId === session.user.id) !== undefined;
+
+    return (
+        <AutosaveIndicatorProvider>
+            <AutoSaveMindMapProvider>
+                <DashboardHeader
+                    addManualRoutes={[
+                        {
+                            name: 'DASHBOARD',
+                            href: '/dashboard',
+                            useTranslate: true,
+                        },
+                        {
+                            name: workspace.name,
+                            href: `/dashboard/workspace/${workspace_id}`,
+                        },
+                        {
+                            emoji: changeCodeToEmoji(mindMap.emoji),
+                            name: `${mindMap.title ? mindMap.title : 'UNTITLED'}`,
+                            href: '/',
+                            useTranslate: mindMap.title ? false : true,
+                        },
+                    ]}
+                >
+                    {(userRole === 'ADMIN' || userRole === 'OWNER') && <InviteUsers workspace={workspace} />}
+                    <AddTaskShortcut userId={session.user.id} />
+                </DashboardHeader>
+                <main className="flex flex-col gap-2 h-full">
+                    <MindMapPreviewCardWrapper
+                        mindMap={mindMap}
+                        userRole={userRole}
+                        isSavedByUser={isSavedByUser}
+                    >
+                        <MindMap
+                            initialActiveTags={mindMap.tags}
+                            initialInfo={mindMap}
+                            workspaceId={workspace.id}
+                            candEdit={false}
+                        />
+                    </MindMapPreviewCardWrapper>
+                </main>
+            </AutoSaveMindMapProvider>
+        </AutosaveIndicatorProvider>
+    );
 };
 export default MindMapPage;
