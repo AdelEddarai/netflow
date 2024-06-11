d8cd10c6c3e61d8f04680b91a0c70ff9b37592e6
unknown <adeleddarai29@gmail.com>
2024-06-11 11:43:26 +0000

    add room in videochat

--- app/[locale]/dashboard/room/page.tsx
+++ app/[locale]/dashboard/room/page.tsx
@@ -1,44 +1,84 @@
 "use client";
 
-import '@livekit/components-styles';
+import "@livekit/components-styles";
 import {
   LiveKitRoom,
-  VideoConference,
   GridLayout,
   ParticipantTile,
   RoomAudioRenderer,
   ControlBar,
-  Chat,
-  LayoutContextProvider,
   useTracks,
-} from '@livekit/components-react';
-import { Track } from 'livekit-client';
-import { useEffect, useState } from 'react';
+  VideoConference,
+} from "@livekit/components-react";
+import { useEffect, useState } from "react";
+import { Track } from "livekit-client";
+import { useSearchParams } from "next/navigation";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 export default function Page() {
-  // TODO: get user input for room and name
-  const room = "room";
-  const name = "user";
-  const [token, setToken] = useState("");
-  const [isChatVisible, setIsChatVisible] = useState(false);
-  const [error, setError] = useState('');
+  const params = useSearchParams();
 
   useEffect(() => {
-    (async () => {
-      try {
-        const resp = await fetch(
-          `/api/get-participant-token?room=${room}&username=${name}`
-        );
-        const data = await resp.json();
-        setToken(data.token);
-      } catch (e) {
-        console.error(e);
-      }
-    })();
-  }, []);
+    const room = params.get("room");
+    const name = params.get("name");
+    if (room && name) {
+      setRoom(room);
+      setName(name);
+    }
+  }, [params]);
+
+  const [room, setRoom] = useState<string>();
+  const [name, setName] = useState<string>();
+
+  const [token, setToken] = useState("");
+
+  async function getToken() {
+    if (!room || !name) {
+      return;
+    }
+    try {
+      const resp = await fetch(
+        `/api/get-participant-token?room=${room}&username=${name}`
+      );
+      const data = await resp.json();
+      setToken(data.token);
+    } catch (e) {
+      console.error(e);
+    }
+  }
 
   if (token === "") {
-    return <div>Getting token...</div>;
+    return (
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          getToken();
+        }}
+        className="flex flex-col justify-center items-center min-h-screen"
+      >
+        <Input
+          type="text"
+          placeholder="Room"
+          value={room}
+          className="mb-4 ring-1"
+          onChange={(e) => setRoom(e.target.value)}
+        />
+        <Input
+          type="text"
+          placeholder="Name"
+          value={name}
+          className="mb-4 ring-1"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <Button
+          type="submit"
+          className="p-4 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition-colors"
+        >
+          Join
+        </Button>
+      </form>
+    );
   }
 
   return (
@@ -47,13 +87,12 @@ export default function Page() {
       audio={true}
       token={token}
       serverUrl={process.env.NEXT_PUBLIC_LIVEKIT_URL}
+      onDisconnected={() => setToken("")}
       // Use the default LiveKit theme for nice styles.
       data-lk-theme="default"
-      style={{ height: '100dvh' }}
+      style={{ height: "100dvh" }}
     >
-
-  <VideoConference />
-
+        <VideoConference />
     </LiveKitRoom>
   );
 }
@@ -66,10 +105,13 @@ function MyVideoConference() {
       { source: Track.Source.Camera, withPlaceholder: true },
       { source: Track.Source.ScreenShare, withPlaceholder: false },
     ],
-    { onlySubscribed: false },
+    { onlySubscribed: false }
   );
   return (
-    <GridLayout tracks={tracks} style={{ height: 'calc(100vh - var(--lk-control-bar-height))' }}>
+    <GridLayout
+      tracks={tracks}
+      style={{ height: "calc(100vh - var(--lk-control-bar-height))" }}
+    >
       {/* The GridLayout accepts zero or one child. The child is used
       as a template to render all passed in tracks. */}
       <ParticipantTile />
