50bb9606978243a41defc64b27dc1ea0012cc813
unknown <adeleddarai29@gmail.com>
2024-07-25 13:33:15 +0000

    :pen: fix locale issues

--- hooks/useChangeLocale.tsx
+++ hooks/useChangeLocale.tsx
@@ -4,18 +4,19 @@ import { usePathname, useRouter } from 'next/navigation';
 import { useState, useTransition } from 'react';
 
 export const useChangeLocale = () => {
-	const [isLoading, setIsLoading] = useState(false);
-	const [isPending, startTransition] = useTransition();
+    const [isLoading, setIsLoading] = useState(false);
+    const [isPending, startTransition] = useTransition();
 
-	const router = useRouter();
-	const pathname = usePathname();
+    const router = useRouter();
+    const pathname = usePathname();
 
-	const onSelectChange = (nextLocale: 'pl' | 'en') => {
-		setIsLoading(true);
-		startTransition(() => {
-			router.replace(pathname, { locale: nextLocale });
-		});
-	};
+    const onSelectChange = (nextLocale: 'pl' | 'en') => {
+        setIsLoading(true);
+        startTransition(() => {
+            const newPathname = `/${nextLocale}${pathname}`;
+            router.push(newPathname);
+        });
+    };
 
-	return { isLoading, isPending, onSelectChange };
+    return { isLoading, isPending, onSelectChange };
 };
--- i18n.ts
+++ i18n.ts
@@ -4,9 +4,10 @@ import { notFound } from 'next/navigation';
 export const locales = ['en', 'pl'] as const;
 
 export default getRequestConfig(async ({ locale }) => {
-	if (!locales.includes(locale as 'pl' | 'en')) notFound();
+	if (!locales.includes(locale as any)) notFound();
 
 	return {
 		messages: (await import(`./messages/${locale}.json`)).default,
 	};
 });
+
