025efd559361cb019cf0e2584b101afb2dd1d898
unknown <adeleddarai29@gmail.com>
2024-09-12 09:30:58 +0000

    update middleware

--- middleware.ts
+++ middleware.ts
@@ -40,5 +40,5 @@ export default function middleware(req: NextRequest) {
 }
 
 export const config = {
-    matcher: ['/((?!api|_next/static|_next/image|favicon.ico|publish).*)'],
+    matcher: ['/((?!api|_next/static|_next/image|images|favicon.ico|publish).*)'],
   }
